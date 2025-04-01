1af22293c911168dd555747c018b17607a9242ef
Liu Hongde <lhd_1013@163.com>
2025-04-01 09:25:20 +0800

    Update losses.py

--- src/models/losses.py
+++ src/models/losses.py
@@ -1,14 +1,3 @@
-"""
-Derived from the following publication:
-@Article{khosla2020supervised,
-    title   = {Supervised Contrastive Learning},
-    author  = {Prannay Khosla and Piotr Teterwak and Chen Wang and Aaron Sarna and Yonglong Tian and Phillip Isola and Aaron Maschinot and Ce Liu and Dilip Krishnan},
-    journal = {arXiv preprint arXiv:2004.11362},
-    year    = {2020},
-}
-Author: Yonglong Tian (yonglong@mit.edu)
-Date: May 07, 2020
-"""
 from __future__ import print_function
 
 import torch
@@ -16,8 +5,6 @@ import torch.nn as nn
 
 
 class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
     def __init__(self, temperature=0.07, contrast_mode='all',
                  base_temperature=0.07, loss_scaling_factor=1.0):
         super(SupConLoss, self).__init__()
@@ -27,17 +14,6 @@ class SupConLoss(nn.Module):
         self.scaling_factor = loss_scaling_factor
 
     def forward(self, features, labels=None, mask=None):
-        """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-        Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
-            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
-                has the same class as sample i. Can be asymmetric.
-        Returns:
-            A loss scalar.
-        """
         device = (torch.device('cuda')
                   if features.is_cuda
                   else torch.device('cpu'))
